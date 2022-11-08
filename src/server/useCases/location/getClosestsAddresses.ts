dc3747d973f639b5f0a68252513d72e7496b5872
lucas_ccg@hotmail.com <lucas_ccg@hotmail.com>
2022-11-08 17:16:49 -0300

    removing comments

--- src/server/useCases/location/getClosestsAddresses.ts
+++ src/server/useCases/location/getClosestsAddresses.ts
@@ -23,17 +23,8 @@ export class GetClosestsAddresses {
     const allDistances = this._locationFactory.createDistances(
       allDitancesBetweenLocations
     );
-    // distances calcula a distancia mais curta - pegar pelo value
-    // console.log("distancias: ", allDistances.distances);
     const shortestDistance = allDistances.selectShortestDistance();
-    // console.log("shortestDistance: ", shortestDistance);
     const longestDistance = allDistances.selectLongestDistance();
-    // console.log("longestDistance: ", longestDistance);
-
-    // console.log(
-    //   `allDistances: ${allDistances} \n, shortest: ${shortestDistance} \n, longest: ${longestDistance}`
-    // );
-    // retorna a distancia mais proxima, distancia mais longa e todas as distancias
     return { allDistances, shortestDistance, longestDistance };
   }
 }
