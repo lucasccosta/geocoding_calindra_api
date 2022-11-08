c3b6e66bb246e7830b7283cd945006fe0a0a41f8
lucas_ccg@hotmail.com <lucas_ccg@hotmail.com>
2022-11-08 13:15:14 -0300

    removing comments

--- src/server/integrations/geoLocationApi.ts
+++ src/server/integrations/geoLocationApi.ts
@@ -1,14 +1,13 @@
 import axios from "axios";
 import { Address } from "../domain/valueObject/Address";
 
-const BASE_URL = "https://api.geoapify.com/v1/geocode";
+const BASE_URL = "https://maps.googleapis.com/maps/api";
 const LAT_PARAMETER = "7C";
 const LNG_PARAMETER = "2C";
 
 export class GeoLocationApi {
   async getLatAndLonByLocation(address: string) {
     try {
-      // const location = "1600+Amphitheatre+Parkway,+Mountain+View,+CA&";
       const response = await axios.get(
         `${BASE_URL}/geocode/json?address=${address}&key=${process.env.API_KEY}`
       );
@@ -26,8 +25,7 @@ export class GeoLocationApi {
   ) {
     try {
       const response = await axios.get(
-        // `${BASE_URL}/search?text=${location}&apiKey=68203bcb66da4ca6902865d0bea5337c`
-        `${BASE_URL}/distancematrix/json?origins=${firstAddress.lat}%${LNG_PARAMETER}${firstAddress.lng}&destinations=${secondAddress.lat}%${LNG_PARAMETER}${secondAddress.lng}&key=AIzaSyCsrPjQEjemeQAynjBRfgq1-OgKJChS_dA`
+        `${BASE_URL}/distancematrix/json?origins=${firstAddress.lat}%${LNG_PARAMETER}${firstAddress.lng}&destinations=${secondAddress.lat}%${LNG_PARAMETER}${secondAddress.lng}&key=${process.env.API_KEY}`
       );
 
       const elements = response.data.rows[0].elements[0];
