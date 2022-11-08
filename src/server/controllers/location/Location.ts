bb4e70168d0dbc744b13d2baa8ac3a5d93e45a13
lucas_ccg@hotmail.com <lucas_ccg@hotmail.com>
2022-11-08 13:11:51 -0300

    add Query as interface of parameter address

--- src/server/controllers/location/Location.ts
+++ src/server/controllers/location/Location.ts
@@ -2,14 +2,23 @@ import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import { GetClosestsAddresses } from "../../useCases/getClosestsAddresses";
 
-export const getLocation = (request: Request, response: Response) => {
+interface Query {
+  address: Array<string>;
+}
+
+export const getLocation = async (request: Request, response: Response) => {
   try {
-    const { addresses } = request.query;
+    const { address } = request.query as unknown as Query;
     const useCase = new GetClosestsAddresses();
-    useCase.execute(addresses);
-    return response.status(StatusCodes.OK).json({});
+    //     mudar nome
+    const addressesResponse = await useCase.execute(address);
+    return response
+      .status(StatusCodes.OK)
+      .json(
+        `${addressesResponse.shortestDistance} ${addressesResponse.longestDistance}, ${addressesResponse.allDistances}`
+      );
   } catch (error) {
-    if (error.algo) {
+    if (error) {
       return response.status(StatusCodes.BAD_REQUEST);
     }
   }
