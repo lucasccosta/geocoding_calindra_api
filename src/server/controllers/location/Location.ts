acd4fca6f9537da6f39cbac2e0086ffc03817dd7
lucas_ccg@hotmail.com <lucas_ccg@hotmail.com>
2022-11-08 17:14:51 -0300

    changig const name to addresses

--- src/server/controllers/location/Location.ts
+++ src/server/controllers/location/Location.ts
@@ -10,13 +10,24 @@ export const getLocation = async (request: Request, response: Response) => {
   try {
     const { address } = request.query as unknown as Query;
     const useCase = new GetClosestsAddresses();
-    //     mudar nome
-    const addressesResponse = await useCase.execute(address);
-    return response
-      .status(StatusCodes.OK)
-      .json(
-        `${addressesResponse.shortestDistance} ${addressesResponse.longestDistance}, ${addressesResponse.allDistances}`
-      );
+    const addresses = await useCase.execute(address);
+    return response.status(StatusCodes.OK).json(
+      `A distancia mais curta é entre os endereços: ${
+        addresses.shortestDistance?.firstAddress.address
+      } e ${
+        addresses.shortestDistance?.secondAddress.address
+      }, com duração de ${addresses.shortestDistance?.duration.text}
+
+       A distancia mais longa é entre os endereços: ${
+         addresses.longestDistance?.firstAddress.address
+       } e ${
+        addresses.longestDistance?.secondAddress.address
+      }, com duração de ${addresses.longestDistance?.duration.text}
+      
+      e a relação de endereços e suas distancias entre si: ${JSON.stringify(
+        addresses.allDistances.distances
+      )}`
+    );
   } catch (error) {
     if (error) {
       return response.status(StatusCodes.BAD_REQUEST);
