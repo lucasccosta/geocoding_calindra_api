5ee84cb61054f3452d174284399232f55f1c921c
lucas_ccg@hotmail.com <lucas_ccg@hotmail.com>
2022-11-08 15:07:15 -0300

    removing comments and transfering service methods to its proper class

--- src/server/useCases/getClosestsAddresses.ts
+++ src/server/useCases/getClosestsAddresses.ts
@@ -1,77 +1,34 @@
 import { Distances } from "../domain/Distances";
-import { Address } from "../domain/valueObject/Address";
 import { AddressDistance } from "../domain/valueObject/AddressDistance";
-import { DistanceBetween } from "../domain/valueObject/DistanceBetween";
-import { GeoLocationApi } from "../integrations/geoLocationApi";
+import { GeolocationService } from "../shared/services/geolocation_service";
 
 export class GetClosestsAddresses {
-  _api: GeoLocationApi;
+  private _locationService: GeolocationService;
 
   constructor() {
-    this._api = new GeoLocationApi();
+    this._locationService = new GeolocationService();
   }
 
-  async execute(addresses: Array<string>) {
-    const allLocations = await this.getAllLocations(addresses);
+  async execute(addresses: Array<string>): Promise<{
+    allDistances: Distances;
+    shortestDistance: AddressDistance | undefined;
+    longestDistance: AddressDistance | undefined;
+  }> {
+    const allLocations = await this._locationService.getAllLocations(addresses);
     const allDitancesBetweenLocations =
-      this.getDitancesBetweenLocations(allLocations);
+      await this._locationService.getDitancesBetweenLocations(allLocations);
     const allDistances = new Distances(allDitancesBetweenLocations);
     // distances calcula a distancia mais curta - pegar pelo value
-    allDistances.selectShortestDistance();
+    console.log("distancias: ", allDistances.distances);
+    const shortestDistance = allDistances.selectShortestDistance();
+    console.log("shortestDistance: ", shortestDistance);
+    const longestDistance = allDistances.selectLongestDistance();
+    console.log("longestDistance: ", longestDistance);
 
-    // retorna a distancia e um to string com um textinho
-  }
-
-  // pôr num serviço
-
-  async getAllLocations(locations: Array<string>) {
-    const allLocations: Array<Address> = [];
-    locations.map(async (location: string) => {
-      const fullLocation = await this._api.getLatAndLonByLocation(location);
-      const fullAddress = new Address(
-        location,
-        fullLocation.lat,
-        fullLocation.lng
-      );
-      allLocations.push(fullAddress);
-    });
-    return allLocations;
-  }
-
-  async getDitancesBetweenLocations(allLocations: Array<Address>) {
-    const allDistances: Array<AddressDistance> = [];
-    for (let firstIndex = 0; firstIndex < allLocations.length; firstIndex++) {
-      for (
-        let secondIndex = 0;
-        secondIndex < allLocations.length;
-        secondIndex++
-      ) {
-        const distanceBetweenAddresses =
-          await this._api.getDistanceBetweenAddresses(
-            allLocations[firstIndex],
-            allLocations[secondIndex]
-          );
-        const distance = {
-          text: distanceBetweenAddresses.distance.text,
-          value: distanceBetweenAddresses.distance.value,
-        };
-        // const duration = new DistanceBetween(
-        //   distanceBetweenAddresses.duration.text,
-        //   distanceBetweenAddresses.duration.value,
-        //   "duration"
-        // );
-        const duration = {
-          text: distanceBetweenAddresses.duration.text,
-          value: distanceBetweenAddresses.duration.value,
-        };
-        const addressDistance = new AddressDistance(
-          distance,
-          duration,
-          allLocations[firstIndex],
-          allLocations[secondIndex]
-        );
-        allDistances.push(addressDistance);
-      }
-    }
+    // console.log(
+    //   `allDistances: ${allDistances} \n, shortest: ${shortestDistance} \n, longest: ${longestDistance}`
+    // );
+    // retorna a distancia mais proxima, distancia mais longa e todas as distancias
+    return { allDistances, shortestDistance, longestDistance };
   }
 }
