16c5b7d43970fa4105a89cb2d305f4f6d0c01bc5
lucas_ccg@hotmail.com <lucas_ccg@hotmail.com>
2022-11-09 00:16:31 -0300

    changing logic to reach controller

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
 
-import { LocationsController } from "../controllers/location";
+// import { LocationsController } from "../controllers/location";
+import { LocationsController } from "../controllers/location/LocationsController";
+
+const locationsController = new LocationsController();
 
 const router = Router();
 
@@ -8,6 +11,7 @@ router.get("/", (req, res) => {
   return res.send("Teste");
 });
 
-router.get("/location", LocationsController.getLocation);
+// router.get("/location", LocationsController.getLocation);
+router.get("/location", locationsController.getDistances);
 
 export { router };
