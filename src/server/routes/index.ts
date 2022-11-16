a0d45a22da252d0efcdb074d7ae33e0cbf1a423f
lucas_ccg@hotmail.com <lucas_ccg@hotmail.com>
2022-11-16 08:51:56 -0300

    removing import from the index locations controller (unused)

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,17 +1,11 @@
 import { Router } from "express";
 
-// import { LocationsController } from "../controllers/location";
 import { LocationsController } from "../controllers/location/LocationsController";
 
 const locationsController = new LocationsController();
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  return res.send("Teste");
-});
-
-// router.get("/location", LocationsController.getLocation);
 router.get("/location", locationsController.getDistances);
 
 export { router };
